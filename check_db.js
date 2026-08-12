f1fdba5a59808272de6939b5633bf240d96cf02c
Dharmik-Octfis <dharmik.m@octfis.com>
2026-08-12 16:27:53 +0530

    chore(coreModule): remove one-off debug scripts and captured diff

--- check_db.js
+++
@@ -1,6 +0,0 @@
-const { PrismaClient } = require('./backend/generated/prisma/client/index.js');
-const prisma = new PrismaClient();
-prisma.compositeItemComponent.findMany({ include: { component: true } })
-  .then(r => console.log(JSON.stringify(r, null, 2)))
-  .catch(console.error)
-  .finally(() => prisma.$disconnect());
